ed6a7a1f42a713ccef1800955dd7889fd8ddc87d
Korne Venema <kornevenema@hotmail.com>
2022-03-23 16:40:37 +0100

    preprocessing fingers

--- app/data_processing.py
+++ app/data_processing.py
@@ -1,11 +1,59 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import os
 
 
-def get_labels(data):
-    pass
+def get_labels():
+    # check if finger images are present
+    if not os.path.isdir("fingers/test") or \
+            not os.path.isdir("fingers/train"):
+        print("train and test data is not present")
+        return
+
+    # check if labels folder already exist
+    if not os.path.isdir("labels"):
+        os.mkdir("labels")
+
+    # create test and train labels
+    for t in ["test", "train"]:
+        print(f"getting labels from {t} images")
+        if not os.path.isfile(f"labels/{t}_classes.npy"):
+            labels = [list(item[:-4].split("_")[-1])
+                      for item in os.listdir(f"fingers/{t}")]
+            np.save(f'labels/{t}_classes.npy', labels)
+        else:
+            print(f"{t} labels already exist")
+            continue
+
+
+def pre_process_images():
+    # check if preprocessed images already exist
+    for t in ["test", "train"]:
+        if not os.path.isfile(f"fingers/{t}_preprocessed.npy"):
+            print(f"preprocessing {t} images...")
+            np.save(f"fingers/{t}_preprocessed.npy", np.array([
+                np.array(Image.open(f"fingers/{t}/{file}").resize(
+                    (32, 32))).flatten()
+                for file in os.listdir(f"fingers/{t}")
+            ]))
+        else:
+            print(f"preprocessed {t} images already exist")
+            continue
 
 
 def test():
-    pass
+    print("size of test labels: {0}".format(
+        np.load("labels/test_classes.npy").shape))
+    print("size of train labels: {0}".format(
+        np.load("labels/train_classes.npy").shape))
+    print("size of test images preprocessed: {0}".format(
+        np.load("fingers/test_preprocessed.npy").shape))
+    print("size of test images preprocessed: {0}".format(
+        np.load("fingers/train_preprocessed.npy").shape))
+
+
+if __name__ == '__main__':
+    get_labels()
+    pre_process_images()
+    test()
