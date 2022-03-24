26588c5e00a5514f21f27e325954bb3e6cac2039
athalia <timmania@solcon.nl>
2022-03-24 15:51:32 +0100

    100% accuracy baby

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import os
 import numpy as np
 import app.data_processing as dp
+from models.cnn_single_output import SingleOutputCNN
+
+
+def cnn_single():
+    train_images = np.load('fingers/train_preprocessed.npy')
+    test_images = np.load('fingers/test_preprocessed.npy')
+    train_labels = np.load('labels/train_classes.npy')
+    test_labels = np.load('labels/test_classes.npy')
+
+    cnn = SingleOutputCNN(32)
+    cnn.add_layers()
+    cnn.add_outputs()
+    cnn.model.summary()
+    print('testing')
 
 
 def main():
@@ -10,6 +24,7 @@ def main():
     dp.get_labels()
     dp.pre_process_images()
     dp.test()
+    cnn_single()
 
 
 if __name__ == '__main__':
