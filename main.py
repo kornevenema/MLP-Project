7c78a23414c71d6e135fd36d4d3d089c7bc6c126
amreitsma <a.m.reitsma@student.rug.nl>
2022-03-24 14:54:35 +0100

    made basic svm model class and function to run svm

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import os
 import numpy as np
 import app.data_processing as dp
+from models.svm_model import svm_baseline
+
+
+def svm_single_output():
+    # Opens labels and images and runs svm for 12 labels
+    train_data = np.load("fingers/train_preprocessed.npy")
+    test_data = np.load("fingers/test_preprocessed.npy")
+    train_labels = np.load("fingers/test_preprocessed.npy")
+    test_labels = np.load("fingers/train_preprocessed.npy")
+    flat_test_data = np.array([d.flatten() for d in test_data])
+    flat_train_data = np.array([d.flatten() for d in train_data])
+    # print(flat_test_data.shape)
+    svm = svm_baseline()
+    # svm.fit(train_data, train_labels)
 
 
 def main():
@@ -10,6 +24,7 @@ def main():
     dp.get_labels()
     dp.pre_process_images()
     dp.test()
+    svm_single_output()
 
 
 if __name__ == '__main__':
---
+++ models/svm_model.py
@@ -0,0 +1,17 @@
+import abc
+from sklearn import svm, metrics
+
+class svm_baseline:
+
+    def __init__(self, kernel="linear", **kwargs):
+        self.classifier = svm.SVC(kernel=kernel, **kwargs)
+        self.preds = None
+
+    def fit(self, train_features, train_labels):
+        self.classifier.fit(train_features, train_labels)
+
+    def predict(self, test_features):
+        self.preds = self.classifier.predict(test_features)
+
+    def report_scores(self, true_labels):
+        return metrics.classification_report(true_labels, self.preds)
